ab6e562e2b324d60571751280d5f1674b87a9d3d
[Hiba] <htant016@uottawa.ca>
2023-07-23 23:23:32 -0400

    edit: changed id and classname-notfound page

--- src/NotFound/NotFound.js
+++ src/NotFound/NotFound.js
@@ -5,7 +5,7 @@ import React from 'react';
 
 const NotFound = () => {
   return (
-    <div id='notFound' className='notFound'>
+    <div id='NotFound' className='NotFound'>
        <div className="text col-md-16 text-center"> 
       <h1>404 - Not Found</h1>
       <p>Emm...The page you are looking for does not exist.</p>
